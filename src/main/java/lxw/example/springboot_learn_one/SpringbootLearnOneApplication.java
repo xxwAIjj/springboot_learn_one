1e2dc4fb0e46b8a3d7a834d80514c36ae6ecc098
lvxiaowei <lvxiaowei777@163.com>
2019-02-27 23:20:44 +0800

    测试提交

--- src/main/java/lxw/example/springboot_learn_one/SpringbootLearnOneApplication.java
+++ src/main/java/lxw/example/springboot_learn_one/SpringbootLearnOneApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootLearnOneApplication {
 
     public static void main(String[] args) {
+        System.out.println("工程启动");
         SpringApplication.run(SpringbootLearnOneApplication.class, args);
     }
 
