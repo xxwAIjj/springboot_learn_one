758fd9c3b5ad6f2ed45e64a7ae276fb86f25e592
lvxiaowei <lvxiaowei777@163.com>
2019-02-27 23:12:00 +0800

    新建springboot工程

---
+++ README.md
@@ -0,0 +1,2 @@
+# springboot_learn_one
+springboot学习测试工程（一）
---
+++ src/main/java/lxw/example/springboot_learn_one/SpringbootLearnOneApplication.java
@@ -0,0 +1,13 @@
+package lxw.example.springboot_learn_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearnOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearnOneApplication.class, args);
+    }
+
+}
